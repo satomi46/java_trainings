04cd3e9ffc27bc2086a200fa1685b67e1f25a901
satomi46 <115714427+satomi46@users.noreply.github.com>
2023-01-11 23:25:17 +0900

    Add files via upload

---
+++ If.java
@@ -0,0 +1,15 @@
+package test;
+
+public class If {
+	public static void main(String[] args) {
+		int value = 0;
+		
+		if (value > 0) {
+			System.out.println("value is positive");
+		} else if (value < 0) {
+			System.out.println("value is negative");
+		} else {
+			System.out.println("value is 0");
+		}
+	}
+}
